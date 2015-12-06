8865e41347ce441d5e8dafc36a3f2fb48774028c
Yohan Beschi <yohan.beschi@gmail.com>
2015-12-06 13:57:00 +0100

    ClassFile and Method builders (init)

---
+++ src/main/java/org/isk/pjba/builder/ClassFileBuilder.java
@@ -0,0 +1,35 @@
+package org.isk.pjba.builder;
+
+import org.isk.pjba.structure.ClassFile;
+import org.isk.pjba.structure.Method;
+
+public class ClassFileBuilder {
+  private final ClassFile classFile;
+
+  public ClassFileBuilder(final int classModifiers, final String fullyQualifiedName) {
+    super();
+    this.classFile = new ClassFile(fullyQualifiedName);
+    this.classFile.addAccessFlags(classModifiers);
+  }
+
+  public MethodBuilder newMethod(final int methodModifiers, final String methodName, final String methodDescriptor) {
+    final int methodIndex = this.classFile.addConstantUTF8(methodName);
+    final int descriptorIndex = this.classFile.addConstantUTF8(methodDescriptor);
+    final Method method = new Method();
+    method.setNameIndex(methodIndex);
+    method.setDescriptorIndex(descriptorIndex);
+    method.setAccessFlags(methodModifiers);
+    this.classFile.addMethod(method);
+    final int parametersCount = method.countParameters(methodDescriptor);
+
+    return new MethodBuilder(this, method, parametersCount);
+  }
+
+  public ClassFile build() {
+    return this.classFile;
+  }
+
+  protected ClassFile getClassFile() {
+    return this.classFile;
+  }
+}
